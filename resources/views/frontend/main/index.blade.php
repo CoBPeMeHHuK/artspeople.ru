571b9639a8814aa5e0f155a52bac1a3ef14a860c
cobpemehhuk_home <gotka_gva@mail.ru>
2020-01-04 16:45:27 +0300

    add_network_icons

--- resources/views/frontend/main/index.blade.php
+++ resources/views/frontend/main/index.blade.php
@@ -126,8 +126,10 @@
 <footer>
 	<div class="footer">
 		<a href="#"><i class="fa fa-facebook"></i></a>
-		<a href="#"><i class="fa fa-facebook"></i></a>
-		<a href="#"><i class="fa fa-facebook"></i></a>
+		<a href="#"><i class="fa fa-instagram"></i></a>
+		<a href="#"><i class="fa fa-twitter"></i></a>
+		<a href="#"><i class="fa fa-pinterest"></i></a>
+		<a href="#"><i class="fa fa-vk"></i></a>
 	</div>
 </footer>
 
