1ce268fc269c786b49bbc1e5f3928bd9fe1c8d5a
cobpemehhuk_home <gotka_gva@mail.ru>
2020-01-06 13:17:32 +0300

    fix

--- app/Services/Api/UserService.php
+++ app/Services/Api/UserService.php
@@ -20,7 +20,7 @@ class UserService extends AppService
     {
         parent::__construct();
     }
-    
+
     public function getAuthUserInformation(){
     	Auth::check() ? $user = $this->getAuthUser() : $user = false;
     	return $user;
@@ -31,7 +31,7 @@ class UserService extends AppService
         $user = $this->getUser($id);
         $avatar  = $this->getAvatar($id);
         $statuses = FriendsRequest::FRIEND_REQUEST_STATUSES;
-        
+
         if($authId != false){
 	        $status = FriendsRequest::where([['first_user',$authId],['second_user',$id]])
 		        ->orWhere([['first_user',$id],['second_user',$authId]])
@@ -41,7 +41,7 @@ class UserService extends AppService
         } else{
         	$status = ['status'=>FriendsRequest::FRIEND_REQUEST_STATUSES[0]];
         }
-        
+
 
         $result = [
         	'id'=>$id,
@@ -56,18 +56,18 @@ class UserService extends AppService
         ];
         return json_encode($result);
     }
-	
+
 	public function getUserWorks($id){
-		
+
     	$works = Work::where('user_id',$id)
 			->orderBy('id','desc')
 			->get();
 		$worksArray =[];
 		$idNumber = 0;
-		
+
 		foreach($works as $work ) {
 			foreach($work->images as $image)
-				
+
 				$worksArray[] = [
 					'id'=>$work->id,
 					'user'=>$work->user,
@@ -86,58 +86,58 @@ class UserService extends AppService
 		}
 		return json_encode($worksArray);
     }
-	
+
     public function getUserAuthWorks(){
-		
+
     	$this->redirectIfNotAuth(route('login'));
     	$user = Auth::user();
-    	
+
     	return $this->getLikes($user);
     }
-	
+
 	public function getUserAuthLikes(){
-		
+
 		$this->redirectIfNotAuth(route('login'));
 		$user = Auth::user();
-		
+
 		return $this->getLikes($user);
     }
-    
+
 	public function getUserLikes($id){
-		
+
 		$this->redirectIfNotAuth(route('login'));
 		$user = User::where('id',$id)->first();
-		
+
 		return $this->getLikes($user);
 	}
-	
+
 	public function changeAvatar($request){
 		$id = Auth::id();
 		if (!empty($request->file('img'))) {
 			$file = $request->file('img');
 			$size = $file->getSize();
 			$fileName = rand(0, 9999) . $file->hashName();
-			
+
 			$data = [
 				'type' => 'avatars',
 				'id' => $id,
 				'size' => $size,
 				'model' => User::class,
 			];
-			
-			
+
+
 			UploadImagesTrait::deleteImage($id,User::class);
-			
+
 			return  UploadImagesTrait::uploadResizeImage($file, $fileName, $data, 300, 'max');
 		}
-		
+
 		return 'error';
     }
-	
+
 	public function getSettings(){
 		$user = Auth::user();
 		$userInformation = $this->getInformationAboutUser($user->id)->first();
-		
+
 		return [
 			'name'=>$user->name,
 			'surname'=>$user->surname,
@@ -150,19 +150,19 @@ class UserService extends AppService
 			'about'=>$userInformation->about_user
 		];
     }
-	
+
 	public function changeSettings($request){
-		
+
 		$id = Auth::id();
-		$name = $request->name;
-		$surname = $request->surname;
-		$birthday = $request->birthday;
-		$phone = $request->phone;
-		$additionalPhone = $request->additional_phone;
-		$site = $request->site;
-		$quote = $request->quote;
-		$about = $request->about;
-		
+		isset($request->name) ? $name = $request->name : $name ='';
+        isset($request->surname) ? $surname = $request->surname : $surname ='';
+        isset($request->birthday) ? $birthday = $request->birthday : $birthday ='';
+        isset($request->phone) ? $phone = $request->phone : $phone ='';
+        isset($request->additional_phone) ? $additionalPhone = $request->additional_phone : $additionalPhone ='';
+        isset($request->site) ? $site = $request->site : $site ='';
+        isset($request->quote) ? $quote = $request->quote : $quote ='';
+        isset($request->about) ? $about = $request->about : $about ='';
+
 		$user = User::query()
 			->findOrFail($id)
 			->update([
@@ -170,7 +170,7 @@ class UserService extends AppService
 				'surname'=>$surname,
 				'updated_at'=> time()
 			]);
-		
+
 		$userInformation = $this->getInformationAboutUser($id)
 			->update([
 				'date_of_birth'=>$birthday,
@@ -180,7 +180,7 @@ class UserService extends AppService
 				'quote'=>$quote,
 				'about_user'=>$about
 			]);
-		
+
 		if($user && $userInformation){
 			return ([
 				'status'=>'success',
@@ -192,34 +192,34 @@ class UserService extends AppService
 			]);
 		}
 	}
-	
+
 	public function getUsers(){
-  
-		
+
+
 		$user = Auth::check() ? Auth::user() : false;
 		$users = MessageService::getAuthUserMessages() ? MessageService::getAuthUserMessages() :false;
 		$userAvatar = $this->getAvatar(Auth::id());
-		
+
 		$parameters = [
 			'user'=>$user,
 			'userAvatar'=>$userAvatar,
 			'users'=>$users
 		];
-		
+
 		return $parameters;
-		
+
 	}
-	
+
 	public function getAuthSubcategories(){
 		$categoryId = Auth::user()->category_id;
 		$subcategories = SubCategory::where('category_id',$categoryId)->get(['id','title']);
 		return $subcategories;
 	}
-	
+
 	public function getPersonAbout($id){
     	$user = User::findOrFail($id);
 		$userInformation = $this->getInformationAboutUser($id)->first();
-		
+
 		return [
 			'name'=>$user->name,
 			'surname'=>$user->surname,
@@ -232,23 +232,23 @@ class UserService extends AppService
 			'about'=>$userInformation->about_user
 		];
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	/*------------------------------------------ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ--------------------------------------*/
-	
+
 
     private function getUser($id){
         return User::findOrFail($id);
@@ -257,15 +257,15 @@ class UserService extends AppService
     private function getAvatar($id){
         return Image::where(['entity'=>User::class,'element_id'=>$id,'type'=>'avatars'])->first();
     }
-    
+
     private function getWorks($userCategory,$id){
     	$subcategory =  SubCategory::whereHas('works', function ($query) use ($userCategory, $id) {
     		$query->where(['user_id'=>$id, 'category_id'=>$userCategory]);
     	})->get();
-    	
+
     	return $subcategory;
     }
-    
+
     private function redirectIfNotAuth($route){
 	    if(!$this->isAuth){
 		    return redirect($route);
@@ -273,18 +273,18 @@ class UserService extends AppService
 	    	return false;
 	    }
     }
-    
+
     private function getLikes($user){
 	    $userCategory = $user->category_id;
 	    $id = $user->id;
 	    $subcategories = $this->getWorks($userCategory,$id);
-	
+
 	    $result =[];
-	
+
 	    foreach($subcategories as $subcategory){
 		    $works=[];
 		    foreach($subcategory->works as $work ) {
-			
+
 			    $works[] = [
 				    'name'=>$work->name,
 				    'description'=>$work->description,
@@ -304,18 +304,18 @@ class UserService extends AppService
 	    }
 	    return $result;
     }
-    
+
     private function getInformationAboutUser($id){
 	    return UserInformation::where('user_id',$id);
     }
-    
+
     private function getAuthUser(){
     	$user = User::where('id',Auth::id())
 		    ->with('avatar')
 		    ->with('userInformation')
 		    ->first();
-    	
+
     	return $user;
     }
-    
+
 }
