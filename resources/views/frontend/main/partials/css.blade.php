cd52cd371b2f67660cbc03238860dacaa0751655
ivan <ivan@gmail.com>
2020-02-18 04:10:23 -0900

    fix header

--- resources/views/frontend/main/partials/css.blade.php
+++ resources/views/frontend/main/partials/css.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="{{ mix('css/hamburgers.min.css') }}">
+<link rel="stylesheet" href="{{ mix('css/hamburger-profile.min.css') }}">
 <link rel="stylesheet" href="{{ asset('additionally/slick/slick.css') }}">
 <link rel="stylesheet" href="{{ asset('additionally/slick/slick-theme.css') }}">
 <link rel="stylesheet" href="{{ mix('css/animate.css') }}">
